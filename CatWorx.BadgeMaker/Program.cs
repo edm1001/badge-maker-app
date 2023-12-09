44d774d119e7d105ae1dc430488ea49537a251fd
edm1001 <edmer_franciz@yahoo.com>
2023-12-08 22:33:47 -0600

    initial commit

---
+++ CatWorx.BadgeMaker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+List<string> employees = new List<string>() { "adam", "amy" };
+
+employees.Add("barbara");
+employees.Add("billy");
+
+// for loop that consoles all employee
+for (int i = 0; i < employees.Count; i++) 
+{
+  Console.WriteLine(employees[i]);
+}
+    }
+  }
+}
