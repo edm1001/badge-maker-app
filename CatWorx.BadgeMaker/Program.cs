a97f14ed015d62cdbefb3d598fe0e3d89a2ebd3f
edm1001 <edmer_franciz@yahoo.com>
2023-12-12 20:22:15 -0600

    transfer printEmployees method to util.cs

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -29,11 +29,7 @@ namespace CatWorx.BadgeMaker
     }
     static void PrintEmployees(List<Employee> employees)
     {
-      for (int i = 0; i < employees.Count; i++)
-      {
-      string template = "{0,-10}\t{1,-20}\t{2}";
-      Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
-}
+      Util.PrintEmployees(employees);
     }
     static void Main(string[] args)
     {
